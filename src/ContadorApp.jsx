3211d4b4ef0dedf72ce0a450e143bb9b697e584e
Jose Meza <enriquesais123@gmail.com>
2024-01-26 00:02:06 -0500

    eventos

---
+++ src/ContadorApp.jsx
@@ -0,0 +1,15 @@
+
+
+export const ContadorApp = () => {
+  return (
+    <>
+    <h1>jjdjdj</h1>
+     <button onClick={function(event){
+        console.log(event)
+     }}>
+        Soy un botón
+     </button>
+    </>
+   
+  )
+}
