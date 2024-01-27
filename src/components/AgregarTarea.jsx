22d170c966e65defe38eb38b8bf147f99e98ea84
Jose Meza <enriquesais123@gmail.com>
2024-01-27 00:09:56 -0500

    formulario

--- src/components/AgregarTarea.jsx
+++ src/components/AgregarTarea.jsx
@@ -2,15 +2,23 @@ import { useState } from "react"
 
 export const AgregarTarea = () => {
     const [inputvalue, setinputvalue] = useState('')
-    const onInputChange=(event)=>{
+    const onInputChange = (event) => {
         setinputvalue(event.target.value)
     }
+
+    const onSubmit = (event) => {
+        event.preventDefault()
+        console.log(inputvalue)
+    }
     return (
-        <input
-            type="text"
-            placeholder="Ingresa tarea nueva"
-            value={inputvalue}
-            onChange={onInputChange}
-        />
+
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                placeholder="Ingresa tarea nueva"
+                value={inputvalue}
+                onChange={onInputChange}
+            />
+        </form>
     )
 }
